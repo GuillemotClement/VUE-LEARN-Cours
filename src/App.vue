c385dbf3918717d99b368f62aee6d8a512202ed3
Clément Guillemot <guillemot.clement@protonmail.com>
2024-07-31 18:20:18 +0200

    Utilisation event

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <!-- <h1 v-bind:class="h1title">Bonjour</h1> -->
-  <!-- <h1 :class="h1title">Bonjour</h1> -->
-  <!-- <h1 :[key]="h1title">Bonjour</h1> -->
-  <h1 v-bind="obj">Bonjour</h1>
+  <h1 v-on:click="userClick">Hello</h1>
 </template>
+
 <script lang="ts" setup>
-  const h1title = 'monTitre';
-  const key = 'class';
-  const h1id = 'monSuperTitle';
-  const obj = {
-    class: h1title,
-    id: h1id
-  };
+  function userClick(event: MouseEvent) {
+    console.log(event);
+  }
 </script>
+
 <style></style>
