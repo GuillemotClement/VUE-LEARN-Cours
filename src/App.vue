138b9d97cf176ee6f07048ffe7fea838e81ef200
Clément Guillemot <guillemot.clement@protonmail.com>
2024-07-31 19:08:17 +0200

    Récupérer un input d'un user

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <h1 v-on:click="userClick">Hello</h1>
+  <input type="text" @input="userInput" />
 </template>
 
 <script lang="ts" setup>
   function userClick(event: MouseEvent) {
     console.log(event);
+  }
+
+  function userInput(event: Event) {
+    // on viens préciser qu'on utilise un élément input
+    //cela améliorer l'autocomplétion
+    const target = event.target as HTMLInputElement;
+    console.log(target.value);
   }
 </script>
 
