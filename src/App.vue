92a26360239e3fe273c185477255e6e7f5a7959b
Clément Guillemot <guillemot.clement@protonmail.com>
2024-07-31 14:33:24 +0200

    Utilisation de v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <h1 v-bind:class="h1title">Bonjour</h1>
+  <!-- <h1 v-bind:class="h1title">Bonjour</h1> -->
+  <!-- <h1 :class="h1title">Bonjour</h1> -->
+  <!-- <h1 :[key]="h1title">Bonjour</h1> -->
+  <h1 v-bind="obj">Bonjour</h1>
 </template>
 <script lang="ts" setup>
   const h1title = 'monTitre';
+  const key = 'class';
+  const h1id = 'monSuperTitle';
+  const obj = {
+    class: h1title,
+    id: h1id
+  };
 </script>
 <style></style>
