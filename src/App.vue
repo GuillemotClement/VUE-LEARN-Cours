9d9d0cd6415dcb2f47b6a6dce226f99c98823009
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 19:31:16 +0200

    Gestion d'un object avec v-if

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <button @click="myVal = !myVal">Afficher</button>
-  <h1 v-if="myVal">Hello</h1>
+  <!-- on affiche la liste que si on as récupérer les éléments -->
+  <h1>Profil</h1>
+  <!-- avec le v-if, on affiche les éléments une fois récupéré -->
+  <ul v-if="state.user">
+    <!-- on viens protéger avec le ? -->
+    <li>Name : {{ state.user?.name }}</li>
+    <li>Age : {{ state.user?.age }}</li>
+  </ul>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { reactive } from 'vue';
 
-  const myVal = ref(true);
+  const state = reactive<any>({});
+
+  setTimeout(() => {
+    state.user = {
+      name: 'Jean',
+      age: 17
+    };
+  }, 3000);
 </script>
 
 <style scoped lang="scss"></style>
