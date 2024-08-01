d0826e4bac9295a531eb2d2639ff219be7f6323f
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 14:27:22 +0200

    Bind de class

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Hello</h1>
+  <h1 :class="h1class">Hello</h1>
+  <button @click="h1class = 'red'">Change</button>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const h1class = ref('Bonjour');
+</script>
 
 <style scoped>
-  @import './assets/base.css';
+  .red {
+    color: red;
+  }
 </style>
