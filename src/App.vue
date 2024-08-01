5942b8d31bb1ff565b1b1345afedf8e724a3d5e8
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 15:01:55 +0200

    Ajout de l'effet sur un input lors du focus et saisis de données

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <h1 :class="[h1class, h1class2]">Bonjour</h1>
+  <input
+    type="text"
+    :class="{
+      // si l'user saiais quelque chose on ajoute la classe
+      inputOnGoing: inputOnGoing
+    }"
+    @focus="focus = true"
+    @blur="focus = false"
+    v-model="input"
+    class="input"
+  />
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { computed, ref } from 'vue';
 
-  const h1class = ref('hello');
-  const h1class2 = ref('world');
+  const input = ref('');
+
+  const focus = ref(false);
+  // @focus permet d'écouter l'event focus
+  // @blur permet d'écouter si on part du focus
+
+  // si il y a quelque chsose dans le input alors retourne true
+  // on ajoute également si focus est true pour ajouter la classe
+  const inputOnGoing = computed(() => focus.value && input.value.length);
 </script>
 
 <style scoped>
-  .red {
-    color: red;
+  .input {
+    outline: 0;
+    border: 2px solid black;
+    border-radius: 10px;
+  }
+
+  .inputOnGoing {
+    border-color: blue;
   }
 </style>
