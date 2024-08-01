8eaae96ce7287a3bc41dd521b621cb97e518f772
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 08:54:04 +0200

    Utilisation de ref()

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <template>
-  <!-- On vient afficher le nom de l'user dans le h1 -->
-  <h1>Bonjour {{ user.name }}</h1>
-  <h2>Count : {{ state.count }}</h2>
-  <!-- on the flight -->
-  <!-- <button @click="state.count++">Add</button> -->
-  <!-- en utilisant une fonction -->
+  <h2>Count : {{ count }}</h2>
   <button @click="addCount">Add</button>
 </template>
 
 <script lang="ts" setup>
-  import { reactive } from 'vue';
+  //on importe la fonction ref() 
+  import { ref } from 'vue';
 
-  //on rends cette objet reactive en le passant à la fonction reactive()
-  const state = reactive({
-    // on déclare un objet user directement dans la fonction reactive
-    user: {
-      name: 'Tintin',
-      age: 15
-    },
-    count: 0
-  });
+  //on délcare une nouvelle variable et on rend reactif la valeur avec ref()
+  const count = ref(0);
 
-  const { user } = state;
   // la fonction viens incrémenter le count
   function addCount() {
-    state.count++;
+    count.value++;
   }
-
-  // au bout de 2s on vient modifier la valeur name de l'objet user
-  // on viens rendre de nouveau le template pour afficher la mise à jour
-  setTimeout(() => {
-    state.user.name = 'Leo';
-  }, 2000);
 </script>
 
 <style></style>
