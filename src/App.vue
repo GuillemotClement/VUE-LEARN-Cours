ca540da311c9b094a63e6262091c70b5cfd981bd
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 14:50:49 +0200

    Passage d'un array de classe

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <h1
-    :class="{
-      red: h1class
-    }"
-  >
-    Hello
-  </h1>
-  <button @click="h1class = !h1class">Change</button>
+  <h1 :class="[h1class, h1class2]">Bonjour</h1>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
 
-  const h1class = ref(true);
+  const h1class = ref('hello');
+  const h1class2 = ref('world');
 </script>
 
 <style scoped>
