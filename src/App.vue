fa6106f23360f756c3ace8cb48899cf9be349da3
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 10:17:57 +0200

    Affichage de la saisie de l'user

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <!-- on lui passe la référence -->
-  <input type="text" v-model="text">
+  <input type="text" v-model="text" />
+  <!-- on affichage la valeur saisis par l'user -->
+  <h1>{{ text }}</h1>
 </template>
 
-<script lang="ts" setup> 
-import { ref } from 'vue';
+<script lang="ts" setup>
+  import { ref } from 'vue';
 
-// création de la référence initialiser avec une chaîne vide.
-const text = ref('');
- 
+  // création de la référence initialiser avec une chaîne vide.
+  const text = ref('');
 </script>
 
 <style></style>
