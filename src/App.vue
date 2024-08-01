a63009945cf06c7e6bd84913bf74aa70a969c243
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 15:23:04 +0200

    Refacto du gestionnaire de input en rassemblant les ref

--- src/App.vue
+++ src/App.vue
@@ -8,41 +8,38 @@
       inputError,
       inputValid
     }"
-    @focus="focus = true"
-    @blur="focus = false"
-    @click="touched = true"
-    v-model="input"
+    @focus="input.focus = true"
+    @blur="input.focus = false"
+    @click="input.touched = true"
+    v-model="input.value"
     class="input"
   />
 </template>
 
 <script lang="ts" setup>
-  import { computed, ref } from 'vue';
+  import { computed, reactive } from 'vue';
 
-  const input = ref('');
-
-  const focus = ref(false);
-  // @focus permet d'écouter l'event focus
-  // @blur permet d'écouter si on part du focus
-
-  // on ajoute une propriété pour savoir si l'user à cliqué au moins une fois sur l'input
-  // on initialise à false car au début l'user n'as pas touché
-  const touched = ref(false);
+  // on viens regrouper nos 3 références
+  const input = reactive({
+    value: '',
+    focus: false,
+    touched: false
+  });
 
   // si il y a quelque chsose dans le input alors retourne true
   // on ajoute également si focus est true pour ajouter la classe
   // on viens écouter le click sur le input et on change ensuite la valeur de la variable
-  const inputOnGoing = computed(() => focus.value && input.value.length);
+  const inputOnGoing = computed(() => input.focus && input.value.length);
 
   // on déclare la constate pour la gestion d'erreur
   // dans la fonction de rappel on définis les règle qui permette d'ajouter la classe
   // si la valeur saisie est inférieur à 5 alors on applique la bordure rouge
   // on vérifie également que l'on as pas le focus pour déclencher la classe erreur
-  const inputError = computed(() => touched.value && input.value.length < 5 && !focus.value);
+  const inputError = computed(() => input.touched && input.value.length < 5 && !input.focus);
 
   //gestion si l'input est valide
   // on vérifie qu'il n'y a pas d'erreur avec !inputError.value
-  const inputValid = computed(() => touched.value && !inputError.value && !focus.value);
+  const inputValid = computed(() => input.touched && !inputError.value && !input.focus);
 </script>
 
 <style scoped>
