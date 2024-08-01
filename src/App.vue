70fe5276dcbd58094b7b4513e6acde6f7a455658
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-01 12:38:55 +0200

    Utilisation watchEffect() + interface

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,25 @@
 </template>
 
 <script lang="ts" setup>
-  import { reactive, computed, watch, ref } from 'vue';
+  import { reactive, computed, watch, ref, watchEffect } from 'vue';
 
-  const product = reactive({
+  // on vient utiliser une interface pour résoudre le problème de type de la propriété lastModification
+  // on viens créer généralement les interfaces dans un fichier externe
+  interface Product {
+    name: string;
+    quantity: number;
+    priceHT: number;
+    nbrModification: number;
+    lastModification?: number | null;
+  }
+
+  //on précise le nom de l'interface
+  const product = reactive<Product>({
     name: 'books',
     quantity: 3,
     priceHT: 10,
-    nbrModification: 0
+    nbrModification: 0,
+    lastModification: null
   });
 
   const price = ref(0);
@@ -25,15 +37,24 @@
     return product.priceHT * product.quantity * 1.2;
   });
 
-  watch(
+  //on pourras utiliser cette variable pour couper le watch
+  const unwatch = watch(
     // on viens surveiller la propriété + la ref
     //le watch s'execute si la quantity change ou bien le price
     [() => product.quantity, price],
     (newValue, oldValue) => {
       // lors d'une modification de cette propriété on incrémente la valeur
       product.nbrModification++;
+      // on viens couper le watch au premier déclenchement
+      unwatch();
     }
   );
+
+  watchEffect(() => {
+    // on viens exécuter directement la fonction
+    // Date.now() retourne le timestamps du moment ou la méthode est invoqué
+    product.lastModification = Date.now();
+  });
 </script>
 
 <style></style>
