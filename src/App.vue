6652cd2dedbc494cd17b8333f27f93ecb8bdf8b3
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-03 15:22:58 -0400

    Afficher plusieurs fois un éléments

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <ul>
-    <li v-for="(value, key) of car">{{ key }} : {{ value }}</li>
+    <li v-for="n in 10">Bonjour</li>
   </ul>
 </template>
 
 <script lang="ts" setup>
-  import { reactive } from 'vue';
-  const car = {
-    wheel: 4,
-    price: 4000
-  };
+  // import { reactive } from 'vue';
 </script>
 
 <style scoped lang="scss"></style>
